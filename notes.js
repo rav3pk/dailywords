69f897a2deea318f57d552ffd04911757be8d4a0
rav3pk <ravenpk46@gmail.com>
2026-04-09 14:02:30 +0200

    Update notes.js

--- notes.js
+++ notes.js
@@ -1,7 +1,7 @@
 const notes = [
 {
   "date": "2026-04-09",
-  "type": "Frase",
+  "type": "Poema",
   "text": "Sentí tu mano en la mía,\ntu mano de compañera,\ntu voz de niña en mi oído\ncomo una campana nueva,\ncomo una campana virgen\nde un alba de primavera.",
   "signature": "Teamo, perdon"
 }
